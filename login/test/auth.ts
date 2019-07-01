18985824524e2b0da1b345b1baf6f54f35d081a7
Kishan <kishansambhi@hotmail.co.uk>
2019-07-01 19:17:40 +0100

    Add database creation to CI

--- login/test/auth.ts
+++ login/test/auth.ts
@@ -1,13 +1,13 @@
-import { validatePassword } from "../src/auth";
-import authenticate from "../src/auth";
+import { validatePassword, issueJWT } from "../src/auth";
 
 import { expect } from "chai";
 
 import bcrypt from "bcrypt";
+const username = "test_user";
 const password = "abc123$Rykan&";
 let passwordHash: string;
 
-describe("Tests authentication logic", () => {
+describe("Authentication logic", () => {
 	before((done) => {
 		bcrypt.hash(password, 12, (err, hash) => {
 			if (err) { done(err); }
@@ -16,17 +16,33 @@ describe("Tests authentication logic", () => {
 		});
 	});
 
-	it("should return true if a password is valid", async () => {
-		// tslint:disable-next-line:no-unused-expression
-		expect(
-			await validatePassword(password, passwordHash),
-		).to.be.true;
+	describe("Password auth", () => {
+		it("should return true if a password is valid", async () => {
+			// tslint:disable-next-line:no-unused-expression
+			expect(
+				await validatePassword(password, passwordHash),
+			).to.be.true;
+		});
+
+		it("should return false if a password is valid", async () => {
+			// tslint:disable-next-line:no-unused-expression
+			expect(
+				await validatePassword(password + "nowInvalid", passwordHash),
+			).to.be.false;
+		});
 	});
 
-	it("should return false if a password is valid", async () => {
-		// tslint:disable-next-line:no-unused-expression
-		expect(
-			await validatePassword(password + "nowInvalid", passwordHash),
-		).to.be.false;
+	describe("JSON Web Tokens", () => {
+
+		it("should return a valid JWT with an expected payload", () => {
+			// tslint:disable-next-line:no-console
+			console.log("THIS SHOULD EVENTUALLY TEST AGAINST JSON SCHEMA");
+			// tslint:disable-next-line:no-unused-expression
+			expect(issueJWT({
+				username: "rykantester",
+				password,
+				id: "some-uuid",
+			})).to.be.string;
+		});
 	});
 });
