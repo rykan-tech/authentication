95d4eef186a62a5a8b7bb469a16beed5f07de5f1
Kishan <kishansambhi@hotmail.co.uk>
2019-07-21 13:22:05 +0100

    Use maven

---
+++ jwt/src/main/java/com/rykan/auth/jwt/Demo2.java
@@ -0,0 +1,5 @@
+package com.rykan.auth.jwt;
+
+public class Demo2 {
+    public static String Test = "0";
+}
---
+++ jwt/src/main/java/com/rykan/auth/jwt/Demo3.java
@@ -0,0 +1,5 @@
+package com.rykan.auth.jwt;
+
+public class Demo3 {
+    public static String Test = "0";
+}
